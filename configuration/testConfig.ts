0df6b176b82fd3a5595a7a6633cdd7c240a9f100
vkaraahmet <vkaraahmet@hepsiemlak.com>
2022-09-15 11:49:34 +0300

    gitignore

--- configuration/testConfig.ts
+++
@@ -1,7 +0,0 @@
-export const testConfig = {
-    loginUrl: `https://giris.hepsiemlak.com/giris-yap`,
-    myAccountUrl: `https://hesabim.hepsiemlak.com/v2/anasayfa`,
-    username: `myaccount@yopmail.com`,
-    password: `123456`,
-    
-}
